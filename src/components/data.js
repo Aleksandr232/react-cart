1ff94da4cc40b9b34545697f6f9f951702036e8b
Aleksandr232 <aleksrowing232@gmail.com>
2022-06-24 20:25:06 +0300

    data

--- src/components/data.js
+++ src/components/data.js
@@ -1,35 +1,38 @@
-
+import crysis from './img/crysis.jpg';
+import ass from './img/ass.jpg';
+import cyber from './img/cyber.jpg';
+import gta5 from './img/gta5.jpg';
 
 
 const data={
     productData:[
         {
             id:1,
-            img: ,
-            title: ,
-            desc: ,
-            price: ,
+            img:crysis ,
+            title:'Crysis' ,
+            desc:'Экшен' ,
+            price:1000 ,
         },
         {
             id:2,
-            img: ,
-            title: ,
-            desc: ,
-            price: ,
+            img:ass,
+            title:'Assassin creed',
+            desc:'Стелс-экшен' ,
+            price:2000 ,
         },
         {
             id:3,
-            img: ,
-            title: ,
-            desc: ,
-            price: ,
+            img:cyber, 
+            title:'Gta 5' ,
+            desc:'Экшен' ,
+            price:2000 ,
         },
         {
             id:4,
-            img: ,
-            title: ,
-            desc: ,
-            price: ,
+            img:gta5 ,
+            title:'Cyberpunk 2077' ,
+            desc:'Экшен',
+            price:2000,
         }
     ]
     
