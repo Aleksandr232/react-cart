3a1f150baa7f42abc9b69ef88da7c8c5b696e5d3
Aleksandr232 <aleksrowing232@gmail.com>
2022-06-24 18:46:54 +0300

    data

---
+++ src/components/data.js
@@ -0,0 +1,39 @@
+
+
+
+const data={
+    productData:[
+        {
+            id:1,
+            img: ,
+            title: ,
+            desc: ,
+            price: ,
+        },
+        {
+            id:2,
+            img: ,
+            title: ,
+            desc: ,
+            price: ,
+        },
+        {
+            id:3,
+            img: ,
+            title: ,
+            desc: ,
+            price: ,
+        },
+        {
+            id:4,
+            img: ,
+            title: ,
+            desc: ,
+            price: ,
+        }
+    ]
+    
+    
+}
+
+export default data;
